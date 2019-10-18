fd5190a64f196c5d915ce637e25be0b528e7f6ee
Dawid Kryszylowicz <d.kryszylowicz@gmail.com>
2019-10-18 23:57:20 +0200

    added deletePost function

--- src/app.js
+++ src/app.js
@@ -4,13 +4,15 @@ import { ui } from './ui';
 document.addEventListener('DOMContentLoaded', getPosts);
 document.querySelector('.post-submit').addEventListener('click', submitPost);
 
+document.querySelector('.posts').addEventListener('click', deletePost)
+
 function getPosts() {
     http.get('http://localhost:3000/posts')
     .then(data => ui.showPosts(data))
     .catch(err => console.log(err));
 }
 
-function submitPost(e) {
+function submitPost() {
     
     const title = document.querySelector('#title').value;
     const body = document.querySelector('#body').value;
@@ -21,7 +23,24 @@ function submitPost(e) {
     }
 
     http.post('http://localhost:3000/posts', data)
-    .then(getPosts())
+    .then(() => {
+        ui.showAlert('Post added', 'alert alert-success');
+        getPosts()
+    })
     .catch(err => console.log(err))
+}
+
+function deletePost(e) {
+    if (e.target.parentElement.classList.contains('delete')) {
+        const id = e.target.parentElement.dataset.id;
+        if (confirm('Are you sure?')) {
+            http.delete(`http://localhost:3000/posts/${id}`)
+            .then(() => {
+                ui.showAlert('Post removed', 'alert alert-success');
+                getPosts();
+            })
+            .catch(err => console.log(err))
+        }
+    }
     e.preventDefault();
 }
